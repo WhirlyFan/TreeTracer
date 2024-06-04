3bdeff2b6f399379651082572758d4434f7f02e8
Michael Lee <mhykoly@gmail.com>
2024-06-03 17:05:59 -0700

    Update Seeds to Accurate Tree Owners

--- app/seeds/members_and_trees.py
+++ app/seeds/members_and_trees.py
@@ -14,28 +14,28 @@ def seed_members_and_trees():
     cousin3 = Member(first_name="Jeffery", last_name="Doe", alternate_name="Cousin Jeffery", picture=silhouette_portrait)
     dad = Member(first_name="Joe", last_name="Smith", alternate_name="Dad", picture=silhouette_portrait)
     mom = Member(first_name="Joana", last_name="Smith", alternate_name="Mom", picture=silhouette_portrait)
-    brother = Member(first_name="Jim", last_name="Smith", alternate_name="Brother Jim", picture=silhouette_portrait)
-    sister = Member(first_name="Jill", last_name="Smith", alternate_name="Sister Jill", picture=silhouette_portrait)
-    ego = Member(first_name="Jacob", last_name="Smith", alternate_name="Ego", picture=silhouette_portrait)
+    demo = Member(first_name="Demo", last_name="Test", alternate_name="Demo", picture=silhouette_portrait)
+    marnie = Member(first_name="Marnie", last_name="Test", alternate_name="Marnie", picture=silhouette_portrait)
+    bobbie = Member(first_name="Bobbie", last_name="Test", alternate_name="Bobbie", picture=silhouette_portrait)
 
     aunt1.add_parents([grandma1, grandpa1])
     cousin1.add_parents([aunt1, uncle1])
     cousin2.add_parents([aunt1, uncle1])
     cousin3.add_parents([aunt1, uncle1])
     dad.add_parents([grandma1, grandpa1])
-    brother.add_parents([dad, mom])
-    sister.add_parents([dad, mom])
-    ego.add_parents([dad, mom])
+    marnie.add_parents([dad, mom])
+    bobbie.add_parents([dad, mom])
+    demo.add_parents([dad, mom])
 
     tree1 = Tree(user_id=1, name="Demo Tree")
     tree2 = Tree(user_id=2, name="Marnie's Tree")
     tree3 = Tree(user_id=3, name="Bobbie's Tree")
 
-    tree1.add_members([grandma1, grandpa1, aunt1, uncle1, cousin1, cousin2, cousin3, dad, mom, brother, sister, ego])
-    tree2.add_members([aunt1, uncle1, cousin1, cousin2, cousin3])
-    tree3.add_members([dad, mom, brother, sister, ego])
+    tree1.add_members([demo, grandma1, grandpa1, aunt1, uncle1, cousin1, cousin2, cousin3, dad, mom, marnie])
+    tree2.add_members([marnie, aunt1, uncle1, cousin1, cousin2, cousin3])
+    tree3.add_members([bobbie, dad, mom, marnie, demo])
 
-    db.session.add_all([grandma1, grandpa1, aunt1, uncle1, cousin1, cousin2, cousin3, dad, mom, brother, sister, ego, tree1, tree2, tree3])
+    db.session.add_all([demo, marnie, bobbie, grandma1, grandpa1, aunt1, uncle1, cousin1, cousin2, cousin3, dad, mom, tree1, tree2, tree3])
     db.session.commit()
 
 # Uses a raw SQL query to TRUNCATE or DELETE the users table. SQLAlchemy doesn't
